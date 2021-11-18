1b5df2e29616a0c91afcd9d9097e67c9258a50b8
weihanchen0725 <weihanchen0725@gmail.com>
2021-11-17 18:15:14 -0800

    Initial Commit

---
+++ src/main/java/com/demobookstore/bookStore/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.demobookstore.bookStore.controller;
+
+
+import com.demobookstore.bookStore.dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks(){
+        BookDto book = BookDto.builder()
+                .title("My First Book Title")
+                .build();
+        List<BookDto> books = new ArrayList<>();
+        books.add(book);
+
+        return ResponseEntity.ok(books);
+    }
+
+}
